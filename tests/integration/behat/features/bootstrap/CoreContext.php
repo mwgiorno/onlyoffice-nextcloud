91e73e7a4ace9fb533881ff9e31b858f34115930
Serik Ibragimov <tomshooter1106@gmail.com>
2026-03-25 18:49:07 +0500

    Update CoreContext.php

--- tests/integration/behat/features/bootstrap/CoreContext.php
+++ tests/integration/behat/features/bootstrap/CoreContext.php
@@ -18,8 +18,8 @@ use Psr\Http\Message\ResponseInterface;
 
 class CoreContext implements Context
 {
-    private const string TEST_PASSWORD = "password";
-    private const string REGULAR_USER = "editortest";
+    private const TEST_PASSWORD = "password";
+    private const REGULAR_USER = "editortest";
 
     private ResponseInterface $response;
     private ?string $currentUser = null;
